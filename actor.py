f5d27c338527577b7fd9d0343e2f84bdd5fc8141
Rickard Lind <rickard@familjen-lind.se>
2021-08-02 21:18:19 +0200

    - Add response methods to Actor - Add TTL to entries - Read sites from config file - Add status, reset and stop endpoints

--- actor.py
+++ actor.py
@@ -10,34 +10,79 @@ def message(method):
     """
     def decorator(self, *args, **kwargs):
         if self._mailbox:
-            self._mailbox.push([method, args, kwargs])
+            self._mailbox.push([method, args, kwargs, None])
 
     return decorator
+
+
+def response(method):
+    """
+    Decorator for actor methods that waits for a response.
+    """
+    async def decorator(self, *args, **kwargs):
+        if self._mailbox:
+            queue = kore.queue()
+            self._mailbox.push([method, args, kwargs, queue])
+            rv = await queue.pop()
+
+            if isinstance(rv, Exception):
+                raise rv
+
+            return rv
+
+    return decorator
+
+
+class Cancelled(Exception):
+    """
+    Message cancelled.
+    """
+    pass
 
 
 class Actor:
     """
     Base class for actors.
     """
-    def __init__(self):
+    def __init__(self, oneshot=False):
+        self._mailbox = None
+        self._waiting = None
+        self._cancelled = False
+        self._oneshot = oneshot
+
+        self.start()
+
+
+    def start(self):
+        """
+        Start actor.
+        """
+        assert self._mailbox is None and self._waiting is None
+
         self._mailbox = kore.queue()
         self._waiting = []
 
         kore.task_create(self._dispatch(self._mailbox))
 
 
-    def die(self):
+    async def stop(self, cancel=False):
         """
-        Tell Actor do die.
+        Stop Actor and wait until it is finished.
         """
-        if self._mailbox:
-            self._mailbox.push([None, None, None])
+        if self._mailbox is not None:
+            if cancel:
+                self._cancelled = True
+            else:
+                self._mailbox.push([None, None, None, None])
+
             self._mailbox = None
 
+        await self.finished()
 
-    async def dead(self):
+
+    async def finished(self):
         """
-        Wait until Actor is dead.
+        Wait for Actor to finish.
         """
         if self._waiting is not None:
             queue = kore.queue()
@@ -52,27 +97,49 @@ class Actor:
         name = self.__class__.__name__
         kore.coroname(name)
 
-        while True:
+        while not self._cancelled:
             try:
-                method, args, kwargs = await mailbox.pop()
+                method, args, kwargs, queue = await mailbox.pop()
 
                 if method is None:
                     break
 
                 if inspect.iscoroutinefunction(method):
                     logging.debug(f'--> await {name}.{method.__name__}')
-                    await method(self, *args, **kwargs)
+                    rv = await method(self, *args, **kwargs)
                 else:
                     logging.debug(f'--> call {name}.{method.__name__}')
-                    method(self, *args, **kwargs)
+                    rv = method(self, *args, **kwargs)
 
-                logging.debug(f'<-- {name}.{method.__name__}')
+                logging.debug(f'<-- {name}.{method.__name__}: rv={rv}')
 
-            except:
+            except Exception as ex:
                 logging.error(f'Exception in {name}._dispatch', exc_info=True)
+                rv = ex
 
+            if queue is not None:
+                queue.push(rv)
+
+            if self._oneshot:
+                break
+
+        for queue in self._drain(mailbox):
+            queue.push(Cancelled())
 
         for queue in self._waiting:
             queue.push(None)
 
         self._waiting = None
+
+
+    def _drain(self, mailbox):
+        """
+        Drain messages from mailbox.
+        """
+        msg = mailbox.popnow()
+
+        while msg is not None:
+            if msg[3] is not None:
+                yield msg[3]
+
+            msg = mailbox.popnow()
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import kore
 
+import json
 import logging
 import sys
+import time
 
 from xml.dom.minidom import parseString
 
@@ -10,94 +12,175 @@ from actor import *
 
 class Rss(Actor):
     """
-    Data model.
+    Query site for RSS feed.
     """
-    URL = {
-        'lwn': 'https://lwn.net/headlines/rss',
-        'bad': 'https://badnews.com/rss',
-    }
+    def __init__(self, site: str, url: str):
+        self._site = site
+        self._url  = url
+        super().__init__()
+
 
     @message
-    async def query(self, site: str, sender: Actor):
+    async def query(self, sender: Actor):
         try:
-            url = self.URL[site]
-            client = kore.httpclient(url)
+            client = kore.httpclient(self._url)
             status, body = await client.get()
 
             if status == 200:
-                dom = parseString(body.decode())
-                title = dom.getElementsByTagName('item').item(0).getElementsByTagName('title').item(0).childNodes.item(0).wholeText
-                sender.news_update(site, title)
+                dom   = parseString(body.decode())
+                item  = dom.getElementsByTagName('item').item(0)
+                title = item.getElementsByTagName('title').item(0)
+                text  = title.childNodes.item(0).wholeText
+
+                sender.news_update(self._site, text)
                 return
 
         except:
-            logging.error(f'Error getting news from {site}', exc_info=True)
+            logging.error(f'Error getting news from {self._site}', exc_info=True)
 
-        sender.news_unavailable(site)
-        
-        
+        sender.news_unavailable(self._site)
+
+
+def now() -> float:
+    """
+    Get current time.
+    """
+    return time.clock_gettime(time.CLOCK_MONOTONIC)
+
+
+class Entry:
+    """
+    Site state.
+    """
+    def __init__(self, site: str, url: str):
+        self.site    = site
+        self.url     = url
+        self.latest  = None
+        self.expires = None
+        self.rss     = None
+        self.pending = False
+        self.waiting = []
+
+
+    @classmethod
+    def load(cls, path: str):
+        with open(path, 'r') as fp:
+            for d in json.load(fp):
+                yield cls(**d)
+
 
 class Model(Actor):
     """
     Data model.
     """
-    def __init__(self):
-        self.rss = Rss()
-        self.pending = {}
+    TTL = 5 * 60
+
+    def __init__(self, path: str):
+        self._sites = { e.site: e for e in Entry.load(path) }
         super().__init__()
 
-        
+
+    async def stop(self):
+        await super().stop()
+
+        for entry in self._sites.values():
+            if entry.rss is not None:
+                await entry.rss.stop()
+
+
+    @response
+    def status(self) -> dict:
+        return {
+            e.site: {
+                'latest': e.latest
+            }
+            for e in self._sites.values()
+        }
+
+
+    @response
+    def reset(self):
+        for e in self._sites.values():
+            e.latest = None
+            e.expires = None
+
+
     @message
     def latest_news(self, site: str, sender: Actor):
         """
         Request latest news.
         """
-        waiting = self.pending.setdefault(site, set())
+        entry = self._sites.get(site)
 
-        if not waiting:
-            self.rss.query(site, self)
+        if entry is None:
+            sender.failure(f'unknown site {site}\n')
 
-        waiting.add(sender)
+        elif entry.latest is not None and now() < entry.expires:
+            logging.debug(f'using cached data for {site}')
+            sender.success(entry.latest + '\n')
 
-        
-    @message    
+        else:
+            if entry.rss is None:
+                entry.rss = Rss(entry.site, entry.url)
+
+            if not entry.pending:
+                entry.pending = True
+                entry.rss.query(self)
+
+            entry.waiting.append(sender)
+
+
+    @message
     def news_update(self, site: str, news: str):
         """
         Received news update from site.
         """
-        for sender in self.pending.pop(site, []):
+        entry = self._sites[site]
+
+        entry.latest  = news
+        entry.expires = now() + self.TTL
+        entry.pending = False
+
+        for sender in entry.waiting:
             sender.success(news + '\n')
 
-        
-    @message    
+        entry.waiting.clear()
+
+
+    @message
     def news_unavailable(self, site: str):
         """
         Site currently unavailable.
         """
-        for sender in self.pending.pop(site, []):
+        entry = self._sites[site]
+
+        entry.latest  = None
+        entry.expires = None
+        entry.pending = False
+
+        for sender in entry.waiting:
             sender.failure('no news is bad news\n')
-        
+
+        entry.waiting.clear()
+
 
 class Response(Actor):
     """
-    One trick Actor.
+    Send REST response.
     """
-    def __init__(self, req, site: str, model: Model):
-        self.req = req
-        super().__init__()
-        model.latest_news(site, self)
+    def __init__(self, req):
+        self._req = req
+        super().__init__(oneshot=True)
 
 
     @message
     def success(self, msg: str):
-        self.req.response(200, msg.encode())
-        self.die()
+        self._req.response(200, msg.encode())
 
-        
+
     @message
     def failure(self, err: str):
-        self.req.response(500, err.encode())
-        self.die()
+        self._req.response(500, err.encode())
 
 
 class App:
@@ -108,7 +191,7 @@ class App:
 
             kore.setname('News')
 
-            self.model = Model()
+            self.model = Model('sites.json')
 
             kore.config.workers         = 1
             kore.config.deployment      = 'dev'
@@ -118,16 +201,36 @@ class App:
 
             d = kore.domain('*', attach='default')
 
-            d.route(r'^/(lwn|bad)$', self.news, methods=['get'])
+            d.route(r'^/news/([a-zA-Z0-9]+)$', self.rest_news, methods=['get'])
+            d.route(r'^/status$', self.rest_status, methods=['get'])
+            d.route(r'^/reset$', self.rest_reset, methods=['post'])
+            d.route(r'^/stop$', self.rest_stop, methods=['post'])
 
         except Exception as e:
             logging.error('configure failed', exc_info=True)
             raise
 
 
-    async def news(self, req, site):
-        rsp = Response(req, site, self.model)
-        await rsp.dead()
+    async def rest_news(self, req, site):
+        rsp = Response(req)
+        self.model.latest_news(site, rsp)
+        await rsp.finished()
+
+
+    async def rest_status(self, req):
+        d = await self.model.status()
+        req.response(200, json.dumps(d, indent=4).encode())
+
+
+    async def rest_reset(self, req):
+        await self.model.reset()
+        req.response(200, b'Reset\n')
+
+
+    async def rest_stop(self, req):
+        await self.model.stop()
+        req.response(200, b'Stopped\n')
+        kore.shutdown()
 
 
 koreapp = App()
